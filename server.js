f6347aaa16499f2e46baf5c1c2287920d9425eeb
UA-Viktor <v.korenkov@icloud.com>
2023-10-22 21:28:03 +0300

    start_db_connect

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
+// const app = require("./app");
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+//
+
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Victor:T4ZanD2bO3ye6BVO@cluster0.y1skkrf.mongodb.net/contacts_list?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connect success");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
