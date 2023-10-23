6e04182b083c5686f5c10c3c0e82f75a5cd16ec5
UA-Viktor <v.korenkov@icloud.com>
2023-10-23 11:55:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@
 
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = require("./config");
+
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
