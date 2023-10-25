ebd8e9942f0bbaab6bf01d5b01c542bca2d70039
UA-Viktor <v.korenkov@icloud.com>
2023-10-25 09:32:11 +0300

    update_auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const router = express.Router();
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 // const schemas = require("../../schemas/contacts");
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrl.listContacts);
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/", authenticate, ctrl.listContacts);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
-// router.post("/", validateBody(schemas.addContact), ctrl.addContact);
-router.post("/", ctrl.addContact);
+// router.post("/", validateBody(schemas.addContact), ctrl.addContact); // не могу понять почему не работает validateBody(schemas.addContact)
+router.post("/", authenticate, ctrl.addContact);
 
-// router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
-router.put("/:id", isValidId, ctrl.updateContact);
-router.put("/:id", isValidId, ctrl.updateContact);
+// router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContact); // не могу понять почему не работает validateBody(schemas.addContact)
+router.put("/:id", authenticate, isValidId, ctrl.updateContact);
 
-router.delete("/:id", isValidId, ctrl.removeContact);
+router.delete("/:id", authenticate, isValidId, ctrl.removeContact);
 
 module.exports = router;
